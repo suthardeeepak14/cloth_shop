5d1d0abbe91ca7a0bc2c2d2e5e92a3357d402559
suthardeepak14 <suthardeepak257@gmail.com>
2023-04-06 08:39:00 +0530

    product

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -13,15 +13,15 @@ const Image = styled.img`
 `;
 
 const Info = styled.div`
-position:absolute;
-top:0;
-left:0;
-width:100%;
-height=100%;
-display:flex;
-flex-direction:column;
-align-items:center;
-justify-content:center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Title = styled.h1`
--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "https://i.ibb.co/XsdmR2c/1.png",
+    img: "https://media.istockphoto.com/id/1154649405/photo/concept-of-shopping-purchases-and-sales-of-happy-young-girl-with-packages-on-yellow-background.jpg?s=612x612&w=0&k=20&c=GU5qT3g9M7Om23y02KfFUzMup8_mkQPa6MkBQmWSVyY=",
     title: "SUMMER SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "f5fafd",
@@ -37,5 +37,40 @@ export const categories = [
     id: 3,
     img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id: 2,
+    img: "https://www.pngarts.com/files/5/Plain-Brown-T-Shirt-PNG-Free-Download.png",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://www.vintageindustries.nl/download_front/qlnzfb2103/_overview_large_/1031_Miller_M65_pant_Dark_Khaki.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://www.vintageindustries.nl/download_front/kaylsc2085/_overview_large_/30123_Riggs_Softshell_Jacket_Graphite.png",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
   },
 ];
