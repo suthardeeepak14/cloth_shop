a604a7d91e046adc44f8d89fb666b797fe22768e
suthardeepak14 <suthardeepak257@gmail.com>
2023-04-07 15:10:34 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
-// import Home from "./pages/Home";
-// import ProductList from "./pages/ProductList";
-// import Register from "./pages/Register";
-// import Product from "./pages/Product";
-// import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+
 const App = () => {
-  return <Cart />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/productList/:id" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart/:id" element={<Cart />} />
+        <Route path="/register/:id" element={<Register />} />
+        <Route path="/login/:id" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
