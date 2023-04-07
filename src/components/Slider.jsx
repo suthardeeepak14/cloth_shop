52f3f68aa3fd258f09935b23d5f053026a2a0edc
suthardeepak14 <suthardeepak257@gmail.com>
2023-04-07 08:03:40 +0530

    responsive

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined";
 import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined";
 import { sliderItems } from "../data";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   width: 100%;
@@ -11,6 +12,7 @@ const Container = styled.div`
   display: flex;
   position: relative;
   overflow: hidden;
+  ${mobile({ display: "none" })}
 `;
 
 const Arrow = styled.div`
